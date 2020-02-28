9239f12b8f42e68687be883e852968c8fc6fabf1
e.sineokov <e.sineokov@corp.mail.ru>
2020-02-28 14:02:07 +0300

    add bot.idle() to echo_bot

--- example/echo_bot.py
+++ example/echo_bot.py
@@ -1,14 +1,15 @@
 from bot.bot import Bot
 from bot.handler import MessageHandler
 
-TOKEN = "" #your toke here
+TOKEN = "" #your token here
+
 bot = Bot(token=TOKEN)
-bot.start_polling()
 
 
 def message_cb(bot, event):
-    print(event)
     bot.send_text(chat_id=event.from_chat, text=event.text)
 
 
 bot.dispatcher.add_handler(MessageHandler(callback=message_cb))
+bot.start_polling()
+bot.idle()
