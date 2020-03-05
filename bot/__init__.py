ed286fbc4b587f9a4bccd93a1b18b3e0a00a0209
e.sineokov <e.sineokov@corp.mail.ru>
2020-03-05 19:25:31 +0300

    up version

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 # Set default logging handler to avoid "No handler found" warnings.
 logging.getLogger(__name__).addHandler(logging.NullHandler())
